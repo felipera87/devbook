3868bc43c9529e640819bc46fa93d5110dc5d63a
Felipe Reis de Andrade <felipe.reis@interlinkbrasil.com>
2021-12-23 09:49:31 -0300

    Finished create user method

--- api/src/repositories/users.go
+++ api/src/repositories/users.go
@@ -16,6 +16,24 @@ func NewUserRepository(db *sql.DB) *Users {
 }
 
 // Create adds a new user to the database
-func (u Users) Create(user models.User) (uint64, error) {
-	return 0, nil
+func (repository Users) Create(user models.User) (uint64, error) {
+	statement, err := repository.db.Prepare(
+		"insert into users (name, nick, email, password) values (?, ?, ?, ?)",
+	)
+	if err != nil {
+		return 0, err
+	}
+	defer statement.Close()
+
+	result, err := statement.Exec(user.Name, user.Nick, user.Email, user.Password)
+	if err != nil {
+		return 0, err
+	}
+
+	lastInsertedID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(lastInsertedID), nil
 }
