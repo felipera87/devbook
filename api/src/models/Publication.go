abb7a30dd252651ae1084a7cbd7bd67477c96fc3
Felipe Reis de Andrade <fandrade@Ubimindss-MacBook-Pro.local>
2022-02-13 16:20:56 -0300

    Added format and validate functions to publication

--- api/src/models/Publication.go
+++ api/src/models/Publication.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"strings"
+)
+
 // Publication represents a publication made by an user
 type Publication struct {
 	ID         uint64 `json:"id,omitempty"`
@@ -9,4 +14,32 @@ type Publication struct {
 	AuthorNick string `json:"authorNick,omitempty"`
 	Likes      uint64 `json:"likes"`
 	CreatedAt  uint64 `json:"createdAt,omitempty"`
+}
+
+// Prepare validates and formats all fields necessary for a new register
+func (publication *Publication) Prepare() error {
+	if err := publication.validate(); err != nil {
+		return err
+	}
+
+	publication.format()
+	return nil
+}
+
+func (publication *Publication) validate() error {
+	if publication.Title == "" {
+		return errors.New("title is required")
+	}
+
+	if publication.Content == "" {
+		return errors.New("content is required")
+	}
+
+	return nil
+}
+
+func (publication *Publication) format() {
+	publication.Title = strings.TrimSpace(publication.Title)
+	publication.Content = strings.TrimSpace(publication.Content)
+
 }
